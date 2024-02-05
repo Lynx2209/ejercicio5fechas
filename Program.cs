5a7dbf1423fefcf6b7011e18f2845723027d614e
Lynx2209 <csi23-jconazo@colegioaltair.net>
2024-02-05 12:58:09 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+
+namespace ejercicio5fechas
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Digame la primera fecha (dd/mm/yyyy hh:mm:ss) : ");
+            String fechaDosStr = Console.ReadLine();
+            Console.WriteLine("Digame la primera fecha (dd/mm/yyyy) : ");
+            String fechaUnoStr = Console.ReadLine();
+
+            DateTime fechaUno = DateTime.Parse(fechaUnoStr);
+            DateTime fechaDos = DateTime.Parse(fechaDosStr);
+            TimeSpan diferencia = fechaUno - fechaDos;
+            Console.WriteLine(diferencia);
+
+        }
+    }
+}
